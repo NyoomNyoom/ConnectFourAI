852c5e5e6bab6e82d00b7bcb064ae6f89d0c6135
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-09-19 00:45:04 +1200

    getting some boiler plate code down for the game running

---
+++ project files/connectFourGame.py
@@ -0,0 +1,40 @@
+__author__ = "@NyoomNyoom"
+__email__ = "jacksonnorth1275@gmail.com"
+__version__ = 1.0
+
+class player:
+
+    def __init__(self, playerNum, agent, game):
+        if(playerNum == 1):
+            self.playerColour = "blue"
+        else:
+            self.playerColour = "red"
+        self.agent = agent
+        self.game = game
+
+    def playGame(self):
+
+    def getMove(self):
+        move = self.agent.agentFunction()
+
+
+
+class game:
+
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+        #The game board, its being treated as a matrix :)
+        self.gameBoard = [
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0]
+                    ]
+
+    def checkWinCon(self, player):
+
+        
