f0a52272b479747a1e3e5f0dde71e92298e9b514
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-04-29 00:32:58 +1200

    Writing comments for my tests

--- project files/testGameLogic.py
+++ project files/testGameLogic.py
@@ -4,11 +4,14 @@ from connectFourGame import Game
 class TestGameLogic(unittest.TestCase):
 
     # Testing methods for the legal move checker.
+
+    # Checks on empty board.
     def test_moveCheckerEmpty(self):
         newGame = Game("Player 1", "player 2")
 
         self.assertEqual(newGame.checkMoveLegal(0), True, "Checker assumes legal move is illegal")
 
+    # Checks on the last row of a column.
     def test_moveCheckerLastRow(self):
         newGame = Game("Player 1", "player 2")
         newGame.gameBoard = [
@@ -21,6 +24,7 @@ class TestGameLogic(unittest.TestCase):
 
         self.assertEqual(newGame.checkMoveLegal(0), True, "Checker assumes legal move is illegal")
 
+    # Checks on a full column.
     def test_moveCheckerIllegalMove(self):
         newGame = Game("Player 1", "player 2")
         newGame.gameBoard = [
@@ -33,6 +37,7 @@ class TestGameLogic(unittest.TestCase):
 
         self.assertEqual(newGame.checkMoveLegal(0), False, "Checker assumes illegal move is legal")
 
+    # Checks the last row of the last column.
     def test_moveCheckerLastRowLastCol(self):
         newGame = Game("Player 1", "player 2")
         newGame.gameBoard = [
