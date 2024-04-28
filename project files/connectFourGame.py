f800682f0a98e509381ac1e779ca6eae30f55af7
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-04-29 00:55:48 +1200

    Final commit for tonight. Been working on documentation and some bug fixing

--- project files/connectFourGame.py
+++ project files/connectFourGame.py
@@ -18,15 +18,18 @@ class Player:
 
 class Game:
 
+    # The constructor for my game class.
+    # Takes in a player class for player 1 and player 2. Initialises variables to default settings. Player 1 always
+    # Starts the game.
     def __init__(self, player1, player2):
+        # Variables from parameters.
         self.player1 = player1
         self.player2 = player2
+
+        # Variables with default values each time.
         self.isPlayer1 = True
-
         self.turnsTaken = 0
-
-        # The game board, its being treated as a matrix :)
-        self.gameBoard = [
+        self.gameBoard = [  # The game board, which is being treated as a matrix :)
                         [0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0],
@@ -38,7 +41,7 @@ class Game:
     def checkWinCon(self, player):
         pass
 
-    # Checks if the move is legal, if not.
+    # Checks if the move is legal, returns true if the move is legal, returns false if not.
     def checkMoveLegal(self, column):
         validMove = False
         rowNum = 0
@@ -52,20 +55,20 @@ class Game:
                 break
         return validMove
 
-    # Makes the move in the given row.
+    # Makes the move in the given column if the move is legal. If the move is not legal, recursively call the makeMove
+    # method.
     def makeMove(self, player):
         column = player.getMove()
         rowNum = 0
 
-        if self.checkMoveLegal(column):
-            if self.isPlayer1:
-                self.gameBoard[rowNum][column] = 1
-                self.isPlayer1 = False
-            else:
-                self.gameBoard[rowNum][column] = 2
-                self.isPlayer1 = True
+        ###### NEED TO CHANGE THIS TO WORK OUT WHAT ROW TO PUT COUNTER IN.
+
+        if self.isPlayer1:
+            self.gameBoard[rowNum][column] = 1
+            self.isPlayer1 = False
         else:
-            self.makeMove(player)
+            self.gameBoard[rowNum][column] = 2
+            self.isPlayer1 = True
 
     # A method to check if the there is four counters straight up or down.
     def checkUpDown(self, playerCounter):
