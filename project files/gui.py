1b4516c4587e99744162c5d2aaeaa3aec3ea5fcb
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-05-22 18:51:14 +1200

    Added a basic welcome screen

--- project files/gui.py
+++ project files/gui.py
@@ -8,6 +8,24 @@ class Gui:
         self.window.config(bg="lightgrey")
 
     def startGame(self):
+        def buttonClicked():
+            self.playerSelection()
+
+        textFrame = tk.Frame()
+        buttonFrame = tk.Frame()
+
+        welcomeText = tk.Label(text="Welcome to Connect Four!", master=textFrame)
+        welcomeText.pack()
+
+        informationText = tk.Label(text="Please press the play button below to start.", master=textFrame)
+        informationText.pack()
+
+        playButton = tk.Button(text="Play Game", command=buttonClicked, master=buttonFrame)
+        playButton.pack()
+
+        textFrame.pack()
+        buttonFrame.pack()
+
         self.window.mainloop()
 
     def playerSelection(self):
@@ -19,7 +37,7 @@ class Gui:
     def updateGameScreen(self):
         pass
 
+
 if __name__ == '__main__':
     guiRun = Gui()
     guiRun.startGame()
-    
